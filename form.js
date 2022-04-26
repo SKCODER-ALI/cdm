3aaf7435108bf01a8a30b6bc85ef801bf0ffd1fa
Saahil <saahilaliofficial@gmail.com>
2022-04-27 00:26:31 +0530

    done

--- form.js
+++ form.js
@@ -9,7 +9,7 @@ function sendEmail(){
         Body: "Name : " + document.getElementById("name").value 
         + "<br> Email:" + document.getElementById("email").value 
         + "<br> Mobile Number :" + document.getElementById("mobile").value 
-        + "<br> Query :" + document.getElementById("query").value
+        + "<br> Message from customer :" + document.getElementById("query").value
     }).then(
         message => alert("Your query has been submitted successfully, We will contact you soon")
     );
